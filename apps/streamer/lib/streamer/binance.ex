c5169d4e00fed31a93d8b23fbde0eaa4fb342251
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-01-09 08:55:20 -0700

    Add event handler function

--- apps/streamer/lib/streamer/binance.ex
+++ apps/streamer/lib/streamer/binance.ex
@@ -11,12 +11,29 @@ defmodule Streamer.Binance do
     )
   end
 
-  def handle_frame({type, msg}, state) do
+  def handle_frame({_type, msg}, state) do
     case Jason.decode(msg) do
       {:ok, event} -> handle_event(event, state)
-      {: error} -> throw("Unable to parse msg: #{msg}")
+      {:error, _} -> throw("Unable to parse msg: #{msg}")
     end
 
     {:ok, state}
+  end
+
+  def handle_event(%{"e" => "trade"}, event, state) do
+    trade_event = %Streamer.Binance.TradeEvent{
+      :event_type => event["e"],
+      :event_time => event["E"],
+      :symbol => event["s"],
+      :trade_id => event["t"],
+      :price => event["p"],
+      :quantity => event["q"],
+      :buyer_order_id => event["b"],
+      :seller_order_id => event["a"],
+      :trade_time => event["T"],
+      :buyer_market_maker => event["m"]
+    }
+
+    IO.inspect(trade_event, label: "Trade event received: ")
   end
 end
