66d144381dd1aef57663540ea1767df1dc24dfcf
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-01-09 08:25:08 -0700

    Add event struct and fix format

--- apps/streamer/lib/streamer/binance.ex
+++ apps/streamer/lib/streamer/binance.ex
@@ -14,8 +14,9 @@ defmodule Streamer.Binance do
   def handle_frame({type, msg}, state) do
     case Jason.decode(msg) do
       {:ok, event} -> handle_event(event, state)
-      {error:,  } -> throw("Unable to parse msg: #{msg}")
+      {: error} -> throw("Unable to parse msg: #{msg}")
     end
+
     {:ok, state}
   end
 end
