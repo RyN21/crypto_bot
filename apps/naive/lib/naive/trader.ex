ec4adae8d8750c9afc709a56f764c7e770d3cd86
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-01-10 12:20:44 -0700

    Edit init method

--- apps/naive/lib/naive/trader.ex
+++ apps/naive/lib/naive/trader.ex
@@ -12,11 +12,17 @@ defmodule Naive.Trader do
     ]
   end
 
-  def start_link(args) do
+  def start_link(%{} = args) do
     GenServer.start_link(__MODULE__, args, name: :trader)
   end
 
-  def init(args) do
-    {:ok, args}
+  def init(%{} = args) do
+    tick_size = fetch_tick_size(args.symbol)
+
+    {:ok, %State{
+      symbol: args.symbol,
+      profit_interval: args.profit_interval,
+      tick_size: tick_size
+      }}
   end
 end
