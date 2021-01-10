329c65ee7aa2a1140e0bf1e11c16fd348f40744a
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-01-10 12:31:23 -0700

    Add fetch tick size method

--- apps/naive/lib/naive/trader.ex
+++ apps/naive/lib/naive/trader.ex
@@ -19,10 +19,25 @@ defmodule Naive.Trader do
   def init(%{} = args) do
     tick_size = fetch_tick_size(args.symbol)
 
-    {:ok, %State{
-      symbol: args.symbol,
-      profit_interval: args.profit_interval,
-      tick_size: tick_size
-      }}
+    {:ok,
+     %State{
+       symbol: args.symbol,
+       profit_interval: args.profit_interval,
+       tick_size: tick_size
+     }}
+  end
+
+  defp fetch_tick_size do
+    %{"filters" => filters} =
+      Binance.get_exchange_info()
+      |> elem(1)
+      |> Map.get(:symbols)
+      |> Enum.find(&(&1["symbol"] == String.upcase(symbol)))
+
+    %{"tickSize" => tick_size} =
+      filters
+      |> Enum.find(&(&1["filterType"] == "PRICE_FILTER"))
+
+    tick_size
   end
 end
